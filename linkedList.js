854b27390a9b4e079a7d9a72a19816a553c7e7cf
liznomura <lizn@hawaii.edu>
2017-06-27 11:23:16 -1000

    created all methods

--- linkedList.js
+++ linkedList.js
@@ -1,8 +1,39 @@
+/*jshint esversion:6*/
 /**
  * @name  linkedListGenerator
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
-  return {};
+function linkedListGenerator() {
+
+  let module = {};
+
+  module.getHead = function() {
+
+  };
+
+  module.getTail = function() {
+
+  };
+
+  module.add = function() {
+
+  };
+
+  module.remove = function() {
+
+  };
+
+  module.get = function() {
+
+  };
+
+  module.insert = function() {
+
+  };
+
+  return module;
 }
+
+
+
